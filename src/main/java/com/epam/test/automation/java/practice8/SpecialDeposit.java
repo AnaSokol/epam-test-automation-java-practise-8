c716ef72e9bb8de840cf559382085ad94cde44e9
HvqEMAOU <hvqemaou@email.com>
2021-05-13 17:42:13 +0000

    Update SpecialDeposit.java

--- src/main/java/com/epam/test/automation/java/practice8/SpecialDeposit.java
+++ src/main/java/com/epam/test/automation/java/practice8/SpecialDeposit.java
@@ -1,10 +1,37 @@
 package com.epam.test.automation.java.practice8;
 
-/**
- * <summary>
- * Implement class according to description of task.
- * </summary>
- */
-public class SpecialDeposit {
-    //TODO: Delete this line and write your own solution;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class SpecialDeposit extends Deposit implements Prolongable {
+
+    public SpecialDeposit(BigDecimal amount, int period) {
+        super(amount, period);
+    }
+
+    @Override
+    public BigDecimal income() {
+        BigDecimal monthlyIncome;
+        BigDecimal depositedSum = getAmount();
+        int period = getPeriod();
+        BigDecimal baseAmount = depositedSum;
+        BigDecimal endSum = depositedSum;
+        BigDecimal bd100 = new BigDecimal(String.valueOf(100));
+        for (int i = 0; i < period; i++){
+            BigDecimal interest = new BigDecimal(String.valueOf(i+1)).divide(bd100);
+            monthlyIncome = (baseAmount.multiply(interest));
+            endSum = baseAmount.add(monthlyIncome);
+            baseAmount = endSum;
+        }
+        BigDecimal totalIncome = endSum.subtract(depositedSum);
+        return totalIncome.setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+    public boolean canToProlong() {
+        BigDecimal minToProlong = new BigDecimal("1000");
+        if (amount.compareTo(minToProlong) > 0) {
+            return true;
+        }
+        return false;
+    }
 }
