4175537d08dc67c12882bb903981e2488b354cb6
HvqEMAOU <hvqemaou@email.com>
2021-05-13 19:51:25 +0000

    Update DepositTest.java

--- src/test/java/com/epam/test/automation/java/practice8/DepositTest.java
+++ src/test/java/com/epam/test/automation/java/practice8/DepositTest.java
@@ -1,5 +1,29 @@
 package com.epam.test.automation.java.practice8;
 
+import org.testng.annotations.Test;
+import java.math.BigDecimal;
+import static org.testng.Assert.*;
+
 public class DepositTest {
-    //TODO: Delete this line and write your own solution;
+    BigDecimal depositAmount = new BigDecimal(String.valueOf(1000));
+    int depositPeriod = 2;
+    BaseDeposit dep1 = new BaseDeposit(depositAmount, depositPeriod);
+
+    @Test
+    public void testGetAmount() {
+         BigDecimal result = dep1.getAmount();
+         BigDecimal expected = depositAmount;
+         assertEquals(result, expected);
+    }
+
+    @Test
+    public void testGetPeriod() {
+        int result = dep1.getPeriod();
+        int expected = depositPeriod;
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void testIncome() {
+    }
 }
