84ea9071970d8cc31216473db55ad31fad2d621d
HvqEMAOU <hvqemaou@email.com>
2021-05-13 17:39:01 +0000

    Update LongDeposit.java

--- src/main/java/com/epam/test/automation/java/practice8/LongDeposit.java
+++ src/main/java/com/epam/test/automation/java/practice8/LongDeposit.java
@@ -1,10 +1,44 @@
 package com.epam.test.automation.java.practice8;
 
-/**
- * <summary>
- * Implement class according to description of task.
- * </summary>
- */
-public class LongDeposit {
-    //TODO: Delete this line and write your own solution;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class LongDeposit extends Deposit implements Prolongable {
+
+    public LongDeposit(BigDecimal amount, int period) {
+        super(amount, period);
+    }
+
+    @Override
+    public BigDecimal income() {
+        BigDecimal monthlyIncome;
+        int period = getPeriod();
+        BigDecimal depositedSum = getAmount();
+        BigDecimal baseAmount = depositedSum;
+        BigDecimal endSum = depositedSum;
+        BigDecimal interest = new BigDecimal(String.valueOf(0.15));
+
+        if (period <= 6){
+            return BigDecimal.ZERO;
+        }
+        else {
+            for (int i = 0; i < period; i++){
+                if (i > 5){
+                    monthlyIncome = (baseAmount.multiply(interest));
+                    endSum = baseAmount.add(monthlyIncome);
+                    baseAmount = endSum;
+                }
+            }
+            BigDecimal totalIncome = endSum.subtract(depositedSum);
+            return totalIncome.setScale(2, RoundingMode.HALF_EVEN);
+        }
+    }
+
+    @Override
+    public boolean canToProlong() {
+        if (period <= 36){
+            return true;
+        }
+        return false;
+    }
 }
