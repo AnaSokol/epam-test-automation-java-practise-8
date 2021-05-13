8c501ee15c990d868c414d357f1593cdc7513ee7
HvqEMAOU <hvqemaou@email.com>
2021-05-13 17:58:45 +0000

    Update BaseDepositTest.java

--- src/test/java/com/epam/test/automation/java/practice8/BaseDepositTest.java
+++ src/test/java/com/epam/test/automation/java/practice8/BaseDepositTest.java
@@ -1,5 +1,36 @@
 package com.epam.test.automation.java.practice8;
 
+import org.testng.annotations.Test;
+import java.math.BigDecimal;
+import static org.testng.Assert.*;
+
 public class BaseDepositTest {
-    //TODO: Delete this line and write your own solution;
+
+    @Test
+    public void testIncome1() {
+        BigDecimal baseAmount = new BigDecimal("3000");
+        int period = 1;
+        BaseDeposit baseDep = new BaseDeposit(baseAmount, period);
+        BigDecimal income = baseDep.income();
+        BigDecimal expected = new BigDecimal("150.00");
+        assertEquals(income, expected);
+    }
+
+    @Test
+    public void testIncome2() {
+        BigDecimal baseAmount = new BigDecimal("3000");
+        BaseDeposit baseDep = new BaseDeposit(baseAmount, 6);
+        BigDecimal income = baseDep.income();
+        BigDecimal expected = new BigDecimal("1020.29");
+        assertEquals(income, expected);
+    }
+
+    @Test
+    public void testIncome3() {
+        BigDecimal baseAmount = new BigDecimal("1000");
+        BaseDeposit baseDep = new BaseDeposit(baseAmount, 3);
+        BigDecimal income = baseDep.income();
+        BigDecimal expected = new BigDecimal("157.62");
+        assertEquals(income, expected);
+    }
 }
