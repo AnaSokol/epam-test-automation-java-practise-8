1e96a45f23df385037f33ceb087e10c5472a4218
HvqEMAOU <hvqemaou@email.com>
2021-05-13 17:37:42 +0000

    Update Deposit.java

--- src/main/java/com/epam/test/automation/java/practice8/Deposit.java
+++ src/main/java/com/epam/test/automation/java/practice8/Deposit.java
@@ -1,10 +1,35 @@
 package com.epam.test.automation.java.practice8;
 
-/**
- * <summary>
- * Implement class according to description of task.
- * </summary>
- */
-public class Deposit {
-    //TODO: Delete this line and write your own solution;
+import java.math.BigDecimal;
+
+public abstract class Deposit implements Comparable<Deposit> {
+    public final BigDecimal amount;
+    public final int period;
+
+    public Deposit (BigDecimal depositAmount, int depositPeriod){
+        amount = depositAmount;
+        period = depositPeriod;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public int getPeriod() {
+        return period;
+    }
+
+    public abstract BigDecimal income ();
+
+    public BigDecimal totalSumAmount(){
+        return income().add(amount);
+    }
+
+    @Override
+    public int compareTo(Deposit o) {
+        if (o != null){
+            return this.totalSumAmount().compareTo(o.totalSumAmount());
+        }
+        return 1;
+    }
 }
